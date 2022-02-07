aaf821aedf1a29a45f8ed8b759d1d07192abba6b
Felipe Dias <adfelipealmeida@gmail.com>
2022-02-07 07:23:51 -0300

    relationship N-N

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +13,28 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            //Produto paoFrances = new Produto();
+            //paoFrances.Nome = "Pão Francês";
+            //paoFrances.PrecoUnitario = 0.40;
+            //paoFrances.Unidade = "Unidade";
+            //paoFrances.Categoria = "Padaria";
+
+            //Compra compra = new Compra();
+            //compra.Quantidade = 6;
+            //compra.Produto = paoFrances;
+            //compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            Promocao promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Pascoa Feliz";
+            promocaoDePascoa.DataDeInicio = DateTime.Now;
+            promocaoDePascoa.DataDeTermino = DateTime.Now.AddMonths(3);
+
+
+            using (LojaContext context = new LojaContext())
+            {
+                //    context.Compras.Add(compra);
+                //    context.SaveChanges();
+            }
         }
     }
 }
